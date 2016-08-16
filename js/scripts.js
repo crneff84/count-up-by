18916e531fb41c51ca2893cce325a5fb6e5da538
Chance Neff and Caleb Stevenson <caleb@>
2016-08-16 10:30:46 -0700

    connect front end to back end

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
-var array = [];
 var totalArray = [];
 var countLoop = function(multiple, endNumber) {
   for (var total = 0; total <= endNumber; total += multiple) {
-    array = totalArray.push(total);
+    totalArray.push(total);
   };
+  return totalArray;
 };
 
 $(function(){
   $("#blanks form").submit(function(event) {
     event.preventDefault();
-
+    var firstInput = parseInt($("input#first-input").val());
+    var secondInput = parseInt($("input#second-input").val());
+    countLoop(firstInput, secondInput);
+    console.log(totalArray);
+    console.log(firstInput);
+    console.log(secondInput);
+    totalArray.forEach(function(result){
+    $("#results").append("<li>" + result + "</li>")
+    });
+    $("#results").show();
   });
-
-
-
 });
